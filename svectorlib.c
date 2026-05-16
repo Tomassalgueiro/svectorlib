41767bda684147a4dcff103c2aab8e151b51371b
guinhas <tomasthor8@gmail.com>
2026-05-16 01:39:07 +0100

    I think main functions are done

--- svectorlib.c
+++ svectorlib.c
@@ -66,10 +66,51 @@ void svector_pop(svector_int *v){
 
 void svector_insert(svector_int *v, int index, int value){
 
+	// index out of bounds
+	if(index < 0 || index > v->size){ 
+		fprintf(stderr, "Index out of bounds");
+		exit(1);
+	}
+
+	if(v->size >= v->capacity){
+		if(v->capacity == 0){
+			v->capacity = 4;
+		}
+		else{
+			v->capacity *=2;
+		}
+
+		int *new_data = realloc(v->data, v->capacity*sizeof(int)); 
+		if(new_data == NULL){
+			fprintf(stderr, "Failed to realloc data");
+			exit(1);
+		}
+
+		v->data = new_data;
+	}
+
+	if (index == v->size) { svector_push(v, value); return; }
+	for(int i = v->size; i > index; i--){
+		v->data[i] = v->data[i-1];
+	}
+	v->data[index] = value;
+	v->size++;
+	return;
+
 }
 
 void svector_remove(svector_int *v, int index){
-
+	if (index < 0 || index >= v->size){
+		fprintf(stderr, "Index out of bounds");
+		exit(1);
+	}
+	
+	if (index == v->size-1) { svector_pop(v); return; }
+	for(int i = index; i < v->size-1; i++){
+		v->data[i] = v->data[i+1];
+	}
+	v->size--;
+	return;
 }
 
 void svector_print(svector_int *v){
